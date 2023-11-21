9a3d9cbe6903dd027d80d452f4464be6caed0312
Henrique_Luan <105437749+gdhenrique303@users.noreply.github.com>
2023-11-21 07:59:13 -0300

    save khavhd

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-//criando/declarando a função tocaSomPom
- function tocaSomPom (){
-    document
- }
+function tocaSom(idElementoAudio){
+   document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSom;
+
+for(let contador = 0;contador < listaDeTeclas.length;contador++);
+   const tecla = listaDeTeclas[contador];
+   const efeito = tecla.classList[1]
+   const idAudio = ´#som_$(efeito)´;
+   tecla.onclick = function(){
+       tocaSom(idAudio)
+   }
+   tecla.onkeydown = function (evento){
+     if (EventSource.code === 'space' | | evento.code ==='Enter'){
+       tecla.classList.add('ativa');
+     }
+     tecla.onkeyup = function(){
+       tecla.classList.remove('ativa')
+     }
+         
+}
